1799f6d2ebfd31919016dff0b18d3015138477be
SnezanaL <snezanalalatovic@gmail.com>
2024-10-30 04:13:11 +0100

    added store dev tool

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,21 @@ import { provideEffects } from '@ngrx/effects';
 import { invoiceReducer } from './state/reducers/invoice.reducer';
 import { InvoiceEffects } from './state/effects/invoice.effects';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore({ invoices: invoiceReducer }),
-    provideEffects(InvoiceEffects),
+    provideEffects([InvoiceEffects]),
     provideHttpClient(),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true, // If set to true, the connection is established within the Angular zone
+    }),
   ],
 };
